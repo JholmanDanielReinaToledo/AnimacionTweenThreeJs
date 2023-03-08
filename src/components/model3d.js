620ece46111a3b1e3aa72c0154f683c15870b02c
Jholman Daniel Reina Toledo <reina.daniel1005@gmail.com>
2023-03-07 21:37:10 -0500

    cubo con animacion y colores

--- src/components/model3d.js
+++ src/components/model3d.js
@@ -1,16 +1,16 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const Model3D = () => {
   const mountRef = useRef(null);
-  const stlLoader = new STLLoader();
 
   useEffect(() => {
     const currentRef = mountRef.current;
     const { clientWidth: width, clientHeight: height } = currentRef;
 
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xC3C4FF);
     const camera = new THREE.PerspectiveCamera(25, width / height, 0.01, 1000);
 
     scene.add(camera);
@@ -22,20 +22,39 @@ const Model3D = () => {
     renderer.setSize(width, height);
     currentRef.appendChild(renderer.domElement);
 
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x0f2c64 });
+    const material = new THREE.MeshPhongMaterial({ color: 0x0f2c64 });
 
     const cube = new THREE.Mesh(geometry, material);
-    // const material = new THREE.MeshPhongMaterial({ color: 0xff5533, specular: 0x111111, shininess: 200 });
-    // Crear malla a partir de la geometría y el material
-    const mesh = new THREE.Mesh(geometry, material);
 
-    this.scene.add(mesh);
     scene.add(cube);
     camera.lookAt(cube.position);
+    
+    const ambientalLight = new THREE.AmbientLight(0x404040, 10);
+    scene.add(ambientalLight);
 
-    renderer.render(scene, camera);
+    const light = new THREE.PointLight(0xff0000, 10);
+    light.position.set(8, 8, 8);
+    scene.add(light);
+
+    const clock = new THREE.Clock();
+
+    const anuimate = () => {
+      const elapsedTime = clock.getElapsedTime();
+      cube.rotation.y = elapsedTime;
+      cube.rotation.x = elapsedTime;
+      cube.position.y = Math.sin(elapsedTime);
+
+      controls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(anuimate);
+
+    }
+    anuimate();
 
     return () => {
       currentRef.removeChild(renderer.domElement);
